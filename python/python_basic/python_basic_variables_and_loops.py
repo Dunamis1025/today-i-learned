0c6c273c1788f0f336e660f2f68ec7b1b72e0f4b
Yunho Shin <yunho1025@gmail.com>
2025-12-21 09:16:01 +1100

    Add basic variables and loops example in Python
    This script demonstrates basic Python concepts including variables, string manipulation, and loops.

---
+++ python/python_basic/python_basic_variables_and_loops.py
@@ -0,0 +1,25 @@
+# Basic coding practice
+
+# Variables
+animal = "dog"
+name = "Yeontan"
+age = 4
+hobby = "walking"
+is_adult = age >= 3
+
+print("Our " + animal + "'s name is " + name)
+hobby = "playing with a ball"
+print(name + " is " + str(age) + " years old, and loves " + hobby)
+print("Is " + name + " an adult? " + str(is_adult))
+
+
+# Practice
+stations = ["Sadang", "Sindorim", "Incheon Airport"]
+for station in stations:
+    print("{0} train is now arriving.".format(station))
+
+
+# Output
+# Sadang train is now arriving.
+# Sindorim train is now arriving.
+# Incheon Airport train is now arriving.
