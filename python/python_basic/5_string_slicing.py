13aa9986b59521de279342ddb6f16aef3afbdd2b
Yunho Shin <yunho1025@gmail.com>
2025-12-22 06:00:24 +1100

    String slicing

--- python/python_basic/5_string_slicing.py
+++ python/python_basic/5_string_slicing.py
@@ -7,6 +7,7 @@ print("Year: " + personal_id[0:2])
 print("Month: " + personal_id[2:4])
 print("Day: " + personal_id[4:6])
 
+
 print("Date of birth: " + personal_id[:6])
 print("Last 7 digits: " + personal_id[7:])
 print("Last 7 digits (from the end): " + personal_id[-7:])
