b81852ce458e058d5880a707a2b022faf2c30276
Yunho Shin <yunho1025@gmail.com>
2025-12-22 05:38:14 +1100

    Add string slicing example in string_slicing_example.py
    Demonstrates string slicing with personal ID.

---
+++ python/python_basic/string_slicing_example.py
@@ -0,0 +1,12 @@
+# String Slicing Example
+
+personal_id = "900101-1234567"
+
+print("Gender code: " + personal_id[7])
+print("Year: " + personal_id[0:2])
+print("Month: " + personal_id[2:4])
+print("Day: " + personal_id[4:6])
+
+print("Date of birth: " + personal_id[:6])
+print("Last 7 digits: " + personal_id[7:])
+print("Last 7 digits (from the end): " + personal_id[-7:])
