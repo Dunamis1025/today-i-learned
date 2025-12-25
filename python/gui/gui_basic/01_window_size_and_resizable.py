380148d59ce999729fad4e0f7ed6935a1df0b744
Yunho Shin <yunho1025@gmail.com>
2025-12-25 17:38:49 +1100

    Basic Tkinter window settings
    This commit demonstrates basic Tkinter window configuration.
    
    - Set the main window title
    - Define window size using geometry (width x height)
    - Optionally set initial window position
    - Disable window resizing for fixed layout practice
    
    This is part of my Tkinter GUI learning journey.

---
+++ python/gui/gui_basic/01_window_size_and_resizable.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+
+# Set window size (width x height)
+root.geometry("640x480")
+
+# Set window size and position (width x height + x_offset + y_offset)
+# root.geometry("640x480+100+300")
+
+# Disable window resizing (width, height)
+root.resizable(False, False)
+
+root.mainloop()
