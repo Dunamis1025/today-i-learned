43963d7e44595cd3f9a635a7f429f0cc074dffa0
Yunho Shin <yunho1025@gmail.com>
2025-12-25 16:12:51 +1100

    For loop basics
    Practiced basic for loop usage in Python.
    Learned how to iterate over a list and use range() with different start and end values.

---
+++ python/python_basic/14_for_loop_basics.py
@@ -0,0 +1,34 @@
+Loop Basics
+
+for queue_no in [1, 2, 3, 4, 5]:
+    print("Queue number: {0}".format(queue_no))
+
+print("")
+
+for queue_no in range(5):
+    print("Queue number: {0}".format(queue_no))
+
+print("")
+
+for queue_no in range(1, 6):
+    print("Queue number: {0}".format(queue_no))
+
+
+Output
+Queue number: 1
+Queue number: 2
+Queue number: 3
+Queue number: 4
+Queue number: 5
+
+Queue number: 0
+Queue number: 1
+Queue number: 2
+Queue number: 3
+Queue number: 4
+
+Queue number: 1
+Queue number: 2
+Queue number: 3
+Queue number: 4
+Queue number: 5
