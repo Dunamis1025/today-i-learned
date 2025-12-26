dcc322c1c3f01c9a7a12afcf8c905ec2bd9708fe
Yunho Shin <yunho1025@gmail.com>
2025-12-27 08:41:04 +1100

    Update 12_ttk_combobox_basic.py

--- 01_python/03_gui/01_gui_basic/12_ttk_combobox_basic.py
+++ 01_python/03_gui/01_gui_basic/12_ttk_combobox_basic.py
@@ -5,7 +5,7 @@ root = Tk()
 root.title("Nado GUI")
 root.geometry("640x480")  # width x height
 
-values = [str(i) + " day" for i in range(1, 32)]  # numbers from 1 to 31
+values = [" Day" + str(i) for i in range(1, 32)]  # numbers from 1 to 31
 combobox = ttk.Combobox(root, height=5, values=values)
 combobox.pack()
 combobox.set("Payment Due Date")  # set default text
